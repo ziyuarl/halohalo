55c7a888bc0e3b484042ae1d6709b8b938e2debf
Mochamad Ziyu Arlian <ziyubaru@gmail.com>
2021-10-01 09:49:41 +0700

    nama saya ziyu

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Mochamad Ziyu Arlian");
+        System.out.println("NIM: 6706213115");
+        System.out.println("Kelas: D3PRLA-45-02");
+    }
+}
